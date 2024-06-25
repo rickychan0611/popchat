b66a24ec3f45e78673417b537b313594e1e2855b
rickychan0611 <ric0611@gmail.com>
2024-06-24 22:06:49 -0700

    detect platform and redirect

--- src/constants/constant.ts
+++ src/constants/constant.ts
@@ -3,3 +3,4 @@ export const H5_URL = "https://aime.hellokit.uk/login"
 export const Android_Download = "https://onlyforyou.live/api/android/download"
 
 export const apiUrl = "http://192.168.0.247:8080"
+
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,10 @@ import "@/styles/globals.css";
 import { NextPage } from "next";
 import type { AppProps } from "next/app";
 import { ReactNode } from "react";
-import { Noto_Sans, Noto_Serif } from 'next/font/google';
+import { Noto_Sans, Noto_Serif } from "next/font/google";
+import { Android_Download, H5_URL } from "@/constants/constant";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 type Page<P = {}> = NextPage<P> & {
   getLayout?: (page: ReactNode) => ReactNode;
@@ -13,28 +16,43 @@ type Props = AppProps & {
   host: any;
 };
 const notoSerif = Noto_Serif({
-  weight: ['400', '700'], // Specify weights you need
-  subsets: ['latin'], // Specify subsets you need
+  weight: ["400", "700"], // Specify weights you need
+  subsets: ["latin"], // Specify subsets you need
 });
 const noto = Noto_Sans({
-  weight: ['400', '700'], // Specify weights you need
-  subsets: ['latin'], // Specify subsets you need
+  weight: ["400", "700"], // Specify weights you need
+  subsets: ["latin"], // Specify subsets you need
 });
 export default function App({ Component, pageProps }: Props) {
-
   //Dynamic layout. Layouts are located in the layouts folder
   const getLayout = Component.getLayout ?? ((page: ReactNode) => page);
+
+  const router = useRouter();
+
+  useEffect(() => {
+    const userAgent = navigator.userAgent;
+
+    // Detect platform
+    let platform = "Unknown";
+
+    if (/android/i.test(userAgent)) {
+      platform = "Android";
+    } else if (/iPad|iPhone|iPod/.test(userAgent)) {
+      platform = "iOS";
+    }
+
+    if (platform === "iOS" || platform === "Android")  router.push(H5_URL)
+    
+  }, []);
 
   return (
     <>
       <style jsx global>{`
         html {
-          font-family: ${noto.style.fontFamily}, ${notoSerif.style.fontFamily}  ;
+          font-family: ${noto.style.fontFamily}, ${notoSerif.style.fontFamily};
         }
       `}</style>
       {getLayout(<Component {...pageProps} />)}
     </>
-  )
-
-
+  );
 }

db11cd3ed35f375df0aa2bca5cbf4f3a2d20bbc0
rickychan0611 <ric0611@gmail.com>
2024-08-18 16:31:24 -0700

    create 1st section

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,10 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous"/>
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@500;700&family=Poppins:wght@400;500;600;700&family=Roboto:wght@300;400;500;700;900&family=Rubik:wght@700&family=Ubuntu:wght@300;400;500&display=swap" rel="stylesheet" />
+
         {/* Add to Home Screen icons */}
         <link rel="apple-touch-icon" sizes="180x180" href="/icons/180.png" />
         <link rel="apple-touch-icon" sizes="152x152" href="/icons/152.png" />
---
+++ src/pages/index-old.tsx
@@ -0,0 +1,449 @@
+import Image from "next/image";
+import { getLayout } from "@/layouts/DefaultLayout";
+import ResponsiveImage from "@/components/ResponsiveImage";
+import { Router } from "lucide-react";
+import { useRouter } from "next/router";
+import { useState } from "react";
+import { Android_Download, H5_URL } from "@/constants/constant";
+import NavMenu from "@/components/NavMenu";
+const Home = () => {
+  const router = useRouter();
+  const [open1, setOpen1] = useState(false);
+  const [open2, setOpen2] = useState(false);
+  const [open3, setOpen3] = useState(false);
+
+  const QRcodeButtons = () => {
+    return (
+      <div className="flex justify-center items-center w-full h-full absolute bg-[rgba(0,0,0,0.7)] rounded-[30px]">
+        <div className="flex flex-row gap-6">
+          <Image
+            src="/qrcode.png"
+            width={120}
+            height={120}
+            alt="qrcode"
+            className="object-contain"
+          />
+        </div>
+        <div className="flex flex-col gap-2 ml-4">
+          <Image
+            src="/andriod.png"
+            width={200}
+            height={55}
+            alt="android"
+            className="object-contain cursor-pointer"
+            onClick={() => {
+              router.push(Android_Download);
+            }}
+          />
+          <Image
+            src="/apple.png"
+            width={200}
+            height={55}
+            alt="ios"
+            className="object-contain cursor-pointer"
+            onClick={() => router.push("/addIcon")}
+          />
+        </div>
+      </div>
+    );
+  };
+
+  return (
+    <div
+      className={`flex min-h-screen flex-col items-center justify-between bg-black `}
+    >
+      <div
+        className="flex relative w-full h-full  flex-col pb-4"
+        style={{
+          backgroundImage: "url('/bg-1.jpg')",
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center top",
+          backgroundSize: "cover",
+        }}
+      >
+        <NavMenu />
+
+      </div>
+
+      {/* <div className="sm:hidden block">
+        <div className="flex flex-row gap-4  mb-2">
+          <Image
+            src="/andriod.png"
+            width={150}
+            height={60}
+            alt="logo"
+            className="object-contain cursor-pointer"
+            onClick={() => router.push(Android_Download)}
+          />
+          <Image
+            src="/apple.png"
+            width={150}
+            height={60}
+            alt="logo"
+            className="object-contain cursor-pointer"
+            onClick={() => router.push("/addIcon")}
+          />
+        </div>
+        <p className="font-serif text-2xl text-white mb-4 text-center">
+          Download now
+        </p>
+      </div> */}
+
+      {/* section 2 */}
+      <div className="bg-black w-full pb-[100px] sm:pb-[200px]">
+        <div className="flex relative w-full h-full flex-col bg-white ">
+          <div
+            className="flex relative w-full h-full flex-row justify-center bg-white pt-[100px] "
+            style={{
+              backgroundImage: "url('/bg-blue.png')",
+              backgroundRepeat: "no-repeat",
+              backgroundPosition: "center top",
+              backgroundSize: "cover",
+            }}
+          >
+            <div className="flex flex-col lg:flex-row relative w-full lg:p-10 max-w-[1300px] ">
+              {/* left side */}
+              <div className="flex flex-col justify-center items-center w-full flex-1">
+                <div className="mt-10 lg:mt-20 lg:mb-20 px-4">
+                  <Image
+                    src="/1on1.png"
+                    width={200}
+                    height={200}
+                    alt="1on1 logo"
+                  />
+                  <div className="flex flex-row">
+                    <div className="w-[6px] h-[170px] relative mr-[34px] mt-4">
+                      <Image
+                        src="/line-v.png"
+                        fill
+                        sizes="100%"
+                        alt="private show"
+                        className="object-fit"
+                      />
+                    </div>
+                    <div>
+                      <p className="text-black text-[54px] font-serif italic">
+                        1-on-1 Live: Private Room
+                      </p>
+                      <p className="text-black text-[24px] mt-[35px] mb-10 lg:mb-20 font-serif">
+                        1-on-1 Live Streaming: Engage in direct face-to-face
+                        interaction with the host
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              {/* right side */}
+              <div className="flex w-full lg:w-[500px] justify-center lg:justify-end relative">
+                <div className="rounded-[40px] w-full max-w-[375px] lg:w-[390px] h-[805px] lg:absolute right-[18px] top-[14px]">
+                  <video
+                    controls={false}
+                    autoPlay
+                    muted
+                    loop
+                    playsInline
+                    className="w-full h-full object-cover rounded-[50px] mt-2 sm:mt-0 "
+                  >
+                    <source src="/1on1.mov" type="video/mp4" />
+                    Your browser does not support the video tag.
+                  </video>
+                </div>
+
+                <div className="rounded-[30px] w-[100%] max-w-[408px] lg:w-[408px] h-[820px] absolute ">
+                  <div className="w-full max-w-[408px] lg:w-[408px] h-full relative">
+                    <Image
+                      src="/frame.png"
+                      fill
+                      sizes="100%"
+                      alt="frame"
+                      className="z-40"
+                    />
+                  </div>
+
+                  <div className="absolute w-[98%] lg:max-w-[320px] bottom-2 lg:ml-[-140px]">
+                    <div className="w-full lg:w-[520px] h-[220px] lg:h-[320px] relative scale-90 z-40 ml-[-16px] lg:ml-0">
+                      <Image
+                        src="/video-graphics.png"
+                        fill
+                        sizes="100%"
+                        alt="frame"
+                        className="z-40"
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* section 3 */}
+      <div
+        className="flex relative w-full h-full flex-col justify-center items-center pb-[100px]"
+        style={{
+          backgroundImage: "url('/bg-3.png')",
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center top",
+          backgroundSize: "cover",
+        }}
+      >
+        {/* Private show */}
+        <div className="flex w-full max-w-7xl flex-col lg:flex-row mt-10 sm:mt-32 mb-20 sm:mb-0 px-4 gap-4">
+          <div className="flex flex-1 justify-center items-center">
+            <div className="rounded-[30px] w-[600px] h-[800px]">
+              <video
+                controls={false}
+                autoPlay
+                muted
+                loop
+                playsInline
+                className="w-full h-full object-cover rounded-[40px]"
+              >
+                <source src="/privateShow.mov" type="video/mp4" />
+                Your browser does not support the video tag.
+              </video>
+            </div>
+          </div>
+
+          <div className="flex flex-1 items-center justify-center mt-10 sm:mt-0 text-white">
+            <div className="flex flex-col">
+              <div className="w-[320px] h-[245px] relative">
+                <Image
+                  src="/privateShow.png"
+                  fill
+                  sizes="100%"
+                  alt="private show"
+                  className="object-contain"
+                />
+              </div>
+
+              <div className="text-[16px] flex flex-col lg:flex-row gap-4 mt-16">
+                <div className="px-6 py-2 rounded-full border border-white cursor-pointer flex justify-center items-start">
+                  Ticketed Entry
+                </div>
+                <div className="px-6 py-2 rounded-full border border-white cursor-pointer flex justify-center items-start">
+                  Guest Limit
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* Exclusive Media Unlock */}
+        <div className="flex w-full max-w-[1200px] flex-col sm:flex-row mt-4 sm:mt-40 p-4 gap-16">
+          <div className="flex flex-1 flex-col">
+            <Image
+              src="/line.png"
+              width={132}
+              height={6}
+              alt="unlock-image"
+              className="object-contain"
+            />
+            <div className=" font-serif text-white italic text-[54px] my-8 ">
+              Exclusive Meida Unlock
+            </div>
+            <div
+              className="w-full h-[560px] sm:h-[750px] relative cursor-pointer"
+              onClick={() => {
+                setOpen1(!open1);
+                setOpen2(false);
+                setOpen3(false);
+              }}
+            >
+              <Image
+                src="/unlock1.png"
+                fill
+                sizes="100%"
+                alt="unlock-image"
+                className="object-cover rounded-[30px]"
+              />
+
+              {!open1 ? (
+                <div className="flex justify-center items-center w-full h-full absolute rounded-[30px]">
+                  <Image
+                    src="/unlock-now.png"
+                    width={266}
+                    height={112}
+                    alt="unlock-image"
+                    className="object-contain"
+                  />
+                </div>
+              ) : (
+                <QRcodeButtons />
+              )}
+            </div>
+          </div>
+
+          <div className="flex flex-1 flex-col">
+            <div
+              className="w-full h-[570px] sm:h-[741px] relative cursor-pointer"
+              onClick={() => {
+                setOpen2(!open2);
+                setOpen1(false);
+                setOpen3(false);
+              }}
+            >
+              <Image
+                src="/custom.png"
+                fill
+                sizes="100%"
+                alt="unlock-image"
+                className="object-cover rounded-[30px]"
+              />
+              {open2 && <QRcodeButtons />}
+            </div>
+            <div className=" font-serif text-white italic text-[54px] my-8 ">
+              Customized Media
+            </div>
+            <div className=" font-serif text-white text-[20px] mb-8">
+              Signed Visuals Tailored by Hosts
+            </div>
+            <Image
+              src="/line.png"
+              width={132}
+              height={6}
+              alt="unlock-image"
+              className="object-contain"
+            />
+          </div>
+        </div>
+
+        {/* Chat with Fans Using an AI Persona */}
+        {/* <div
+          className="mx-4 cursor-pointer"
+          onClick={() => {
+            setOpen3(!open3);
+            setOpen1(false);
+            setOpen2(false);
+          }}
+        >
+          <div className="text-white flex relative w-full max-w-6xl h-full my-40 p-[2px] bg-gradient-to-r from-[#FF1CF7] to-[#00F0FF] rounded-[25px]">
+            <div
+              className="w-full rounded-[22px]"
+              style={{
+                backgroundImage: "url('/bg-4.png')",
+                backgroundRepeat: "no-repeat",
+                backgroundPosition: "center top",
+                backgroundSize: "cover",
+              }}
+            >
+              <div className="flex py-16 px-4 sm:px-20 flex-col-reverse sm:flex-row justify-between items-center">
+                <div className="flex flex-col justify-center w-full sm:w-1/2">
+                  <p className="font-serif text-[52px] italic leading-[60px] mb-6">
+                    Chat with Fans Using an AI Persona
+                  </p>
+                  <p className="mb-2 text-[20px] font-serif">
+                    You can’t chat with 100 fans at once, but "it" can! Plus,
+                    users can chat with AI persona at a low cost
+                  </p>
+                </div>
+
+                <Image
+                  src="/ai-icon.png"
+                  width={232}
+                  height={232}
+                  alt="ai-icon"
+                />
+              </div>
+            </div>
+
+            {open3 && (
+              <div className="flex justify-center sm:justify-end items-center sm:pr-20 w-[calc(100%-4px)] h-[calc(100%-4px)] absolute bg-[rgba(0,0,0,0.7)] rounded-[30px]">
+                <div className="flex flex-row gap-6">
+                  <Image
+                    src="/qrcode.png"
+                    width={120}
+                    height={120}
+                    alt="qrcode"
+                    className="object-contain"
+                  />
+                </div>
+                <div className="flex flex-col gap-2 ml-4">
+                  <Image
+                    src="/andriod.png"
+                    width={200}
+                    height={55}
+                    alt="android"
+                    className="object-contain cursor-pointer"
+                    onClick={() => {
+                      router.push(Android_Download);
+                    }}
+                  />
+                  <Image
+                    src="/apple.png"
+                    width={200}
+                    height={55}
+                    alt="ios"
+                    className="object-contain cursor-pointer"
+                    onClick={() => router.push("/addIcon")}
+                  />
+                </div>
+              </div>
+            )}
+          </div>
+        </div> */}
+      </div>
+
+      {/* section 4 - start to chat */}
+      <div
+        className="flex relative w-full h-full flex-col items-center pb-20 px-4"
+        style={{
+          backgroundImage: "url('/bg-5.png')",
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center bottom",
+          backgroundSize: "cover",
+        }}
+      >
+        <div className="flex flex-row gap-2 w-full max-w-[1000px] justify-center items-center mt-[156px] mb-[164px] px-4">
+          <Image
+            src="/line-l.png"
+            width={120}
+            height={6}
+            alt="line"
+            className="object-contain hidden sm:block"
+          />
+          <p className="flex-1 font-serif text-[55px] italic leading-[50px] text-[#171717] font-bold scale-y-110 text-center">
+            Start to Chat
+          </p>
+          <Image
+            src="/line-r.png"
+            width={120}
+            height={6}
+            alt="line"
+            className="object-contain hidden sm:block"
+          />
+        </div>
+
+        <Image
+          src="/chat1.png"
+          width={523}
+          height={720}
+          alt="ai-icon"
+          className="mb-[240px] cursor-pointer"
+          onClick={() => router.push(H5_URL)}
+        />
+        <Image
+          src="/chat2.png"
+          width={523}
+          height={720}
+          alt="ai-icon"
+          className="mb-[240px] cursor-pointer"
+          onClick={() => router.push(H5_URL)}
+        />
+        <Image
+          src="/chat3.png"
+          width={523}
+          height={720}
+          alt="ai-icon"
+          className="mb-[230px] cursor-pointer"
+          onClick={() => router.push(H5_URL)}
+        />
+      </div>
+    </div>
+  );
+};
+
+Home.getLayout = getLayout;
+export default Home;

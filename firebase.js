33cbb6d1da3f5cc9938af71da91b5c9a51cb645b
rickychan_044e <ric0611@gmail.com>
2024-08-20 12:23:09 -0700

    init firebase

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBZziuf2WYuwVlVTLAhJRCTmF5yWKvqTb0",
+  authDomain: "aime-h5-test.firebaseapp.com",
+  projectId: "aime-h5-test",
+  storageBucket: "aime-h5-test.appspot.com",
+  messagingSenderId: "948948534889",
+  appId: "1:948948534889:web:1f26dcb9b3bd52538b04f8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // output: 'export',
   reactStrictMode: true,
   experimental: {
     scrollRestoration: true,
-  }
+  },
+  // trailingSlash: true, // Optional: ensure trailing slashes for better static file serving
 };
 
 export default nextConfig;
